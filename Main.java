99178fe3ce10b0c235a1a224b0de3cc59d19d7be
khrid0328 <khrid0328@wrdsb.ca>
2020-10-06 16:40:09 +0000

    A5Q1 multiplication table

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ * This program asks the user for an integer. It then creates a “multiplication table” for that number from 1 to 12.
+ * @author Dafna Khripun
  */
 public class Main {
 
@@ -9,7 +10,24 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+
+    // create a scanner for user input
+    Scanner input = new Scanner(System.in);
+
+    // ask the user to enter a number to create a multiplication table for
+    System.out.println("Please enter an integer to create a multiplication table for");
+
+    // create a variable for user input
+    int integer = input.nextInt();
     
+    // use a for loop to create a multiplication table
+    for(int i = 1; i <= 12; i++ ){
+
+      // calculate the product of the integer times the count
+      int product = i * integer;
+
+      // loop action: print the "multiplication line"
+      System.out.println(i + " x " + integer + " = " + product);
+    }
   }
 }
